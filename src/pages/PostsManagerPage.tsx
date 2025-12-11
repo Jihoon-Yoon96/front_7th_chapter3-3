d307477d8adcd49cbf391cfac722d5fce487d0ec
Jihoon-Yoon96 <03470@naver.com>
2025-12-12 02:31:06 +0900

    features : user 분리

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -32,6 +32,7 @@ import { EditPostDialog } from "../features/post/edit/ui/EditPostDialog"
 import { DeletePostButton } from "../features/post/delete/ui/DeletePostButton"
 import { CommentList } from "../features/comment/list/ui/CommentList"
 import { TagFilter } from "../features/tag/filter/ui/TagFilter"
+import { UserDetailModal } from "../features/user/detail/ui/UserDetailModal"
 import { usePosts } from "../entities/post/model/usePost"
 import { useComments } from "../entities/comment/model/useComment"
 import { useTags } from "../entities/tag/model/useTags"
@@ -140,12 +141,6 @@ const PostsManager = () => {
     } catch (error) {
       console.error("사용자 정보 가져오기 오류:", error)
     }
-  }
-
-  // 사용자 모달 닫기 핸들러
-  const handleCloseUserModal = () => {
-    setShowUserModal(false)
-    clearSelectedUser()
   }
 
   useEffect(() => {
@@ -311,40 +306,7 @@ const PostsManager = () => {
         </DialogContent>
       </Dialog>
 
-      {/* 사용자 모달 */}
-      <Dialog open={showUserModal} onOpenChange={handleCloseUserModal}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>사용자 정보</DialogTitle>
-          </DialogHeader>
-          <div className="space-y-4">
-            <img src={selectedUser?.image} alt={selectedUser?.username} className="w-24 h-24 rounded-full mx-auto" />
-            <h3 className="text-xl font-semibold text-center">{selectedUser?.username}</h3>
-            <div className="space-y-2">
-              <p>
-                <strong>이름:</strong> {selectedUser?.firstName} {selectedUser?.lastName}
-              </p>
-              <p>
-                <strong>나이:</strong> {selectedUser?.age}
-              </p>
-              <p>
-                <strong>이메일:</strong> {selectedUser?.email}
-              </p>
-              <p>
-                <strong>전화번호:</strong> {selectedUser?.phone}
-              </p>
-              <p>
-                <strong>주소:</strong> {selectedUser?.address?.address}, {selectedUser?.address?.city}, {
-                  selectedUser?.address?.state
-                }
-              </p>
-              <p>
-                <strong>직장:</strong> {selectedUser?.company?.name} - {selectedUser?.company?.title}
-              </p>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
+      <UserDetailModal open={showUserModal} onOpenChange={setShowUserModal} />
     </Card>
   )
 }
