d885ba4fb957a87dc015d8128812699732f795b7
Jihoon-Yoon96 <03470@naver.com>
2025-12-12 18:35:26 +0900

    gh-pages 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+// src/main.tsx
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from "./App.tsx"
 import "./index.css"
 
@@ -11,9 +12,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
--- src/shared/lib/callAPI.ts
+++ src/shared/lib/callAPI.ts
@@ -1,3 +1,6 @@
+const isDev = import.meta.env.DEV;
+const BASE_URL = isDev ? "/api" : "https://dummyjson.com";
+
 interface RequestOptions extends Omit<RequestInit, 'body'> {
   method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
   body?: object | BodyInit;
@@ -16,7 +19,7 @@ export const callAPI = async <T>(endpoint: string, options?: RequestOptions): Pr
     config.body = JSON.stringify(options.body);
   }
 
-  const response = await fetch(`/api${endpoint}`, config);
+  const response = await fetch(`${BASE_URL}${endpoint}`, config);
 
   if (!response.ok) {
     const errorData = await response.json().catch(() => ({ message: 'Unknown error' }));
